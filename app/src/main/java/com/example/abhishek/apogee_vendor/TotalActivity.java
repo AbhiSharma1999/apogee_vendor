972216be6e83f6f94497796b2da959f9d39277cd
ProcrastinatorPro <abhishek3ic@gmail.com>
2019-03-25 19:48:17 +0530

    OtpCheck

--- app/src/main/java/com/example/abhishek/apogee_vendor/TotalActivity.java
+++ app/src/main/java/com/example/abhishek/apogee_vendor/TotalActivity.java
@@ -3,11 +3,13 @@ package com.example.abhishek.apogee_vendor;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,14 +34,30 @@ public class TotalActivity extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        mDatabase.addValueEventListener(new ValueEventListener() {
+        mDatabase.child("vendors").child(vendorId).child("earnings").addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                Log.d("Total",dataSnapshot.toString());
-                String earnings="Total Earning:Rs."+dataSnapshot.child("vendors").child(vendorId).child("earnings").getValue().toString();
-
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("TotalCheck",dataSnapshot.toString());
+                int earnings = Integer.parseInt(dataSnapshot.getValue().toString());
                 earning.setText(earnings);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("TotalCheck",dataSnapshot.toString());
+                int earnings = Integer.parseInt(dataSnapshot.getValue().toString());
+                earning.setText(earnings);
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
             }
 
             @Override
